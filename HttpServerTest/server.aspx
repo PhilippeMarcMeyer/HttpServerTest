fcbd26d7b0b5d891f243e9900bc51d54d7f06582
Philippe MEYER <p.meyer@mappingcontrol.com>
2019-10-04 16:43:00 +0200

    first commit just a mini web server to external http calls

---
+++ HttpServerTest/server.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="server.aspx.cs" Inherits="HttpServerTest.server" %>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Minimum </title>
+
+    <style>
+        h5 {
+            color: #2e44b9;
+            font-weight: bold;
+            font-size: 24px;
+        }
+
+        .big-sign {
+            font-size: 48px;
+            color: #2e44b9;
+        }
+        #zone{
+            padding:50px;
+        }
+        p{
+            font-weight:bold;
+        }
+    </style>
+</head>
+<body>
+    <div id="zone" style="">
+		    <span class="big-sign">¯\_(ツ)_/¯</span>
+		    <br><br>
+		    <h5>HTTP server</h5>
+		    <p>Work in progress</p>
+		
+    </div>
+</body>
+</html>
---
+++ HttpServerTest/server.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HttpServerTest
+{
+    public partial class server : Page
+    {
+        private static string goodToken = "Boom!";
+
+        [WebMethod]
+        public static string authenticate(string username, string password)
+        {
+         
+        //HttpContext ctx = HttpContext.Current;
+        string token = "";
+            if(username == "alfred" && password == "nobel")
+            {
+                token = goodToken;
+            }
+
+            return token;
+
+        }
+
+        [WebMethod]
+        public static People[] getData()
+        {
+            HttpContext context = HttpContext.Current;
+            string token = context.Request.Headers.Get("X-Api-Token");
+            if(token != null && token == goodToken)
+            {
+                return users;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    public class People
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public int role { get; set; }
+    }
+
+      public static People[] users = new People[]
+        {
+            new People { id = 1, name = "Haleemah Redfern", email = "email1@mail.com", phone = "01111111", role = 1},
+            new People { id = 2, name = "Aya Bostock", email = "email2@mail.com", phone = "01111111", role = 1},
+            new People { id = 3, name = "Sohail Perez", email = "email3@mail.com", phone = "01111111", role = 1},
+            new People { id = 4, name = "Merryn Peck", email = "email4@mail.com", phone = "01111111", role = 2},
+            new People { id = 5, name = "Cairon Reynolds", email = "email5@mail.com", phone = "01111111", role = 3}
+        };
+
+    }
+
+}
